15343e6b6427f3f0f6020aa4dc4d0d4f5ae9943e
Quibly <92229762+Quibly@users.noreply.github.com>
2022-03-08 14:51:40 -0700

    Updated body prep and grow_tail

--- cycle/game/casting/cycle.py
+++ cycle/game/casting/cycle.py
@@ -1,5 +1,4 @@
 import constants
-import random
 from game.casting.actor import Actor
 from game.shared.point import Point
 
@@ -25,43 +24,34 @@ class Cycle(Actor):
         # move all segments
         for segment in self._segments:
             segment.move_next()
-        # update velocities
-        for i in range(len(self._segments) - 1, 0, -1):
-            trailing = self._segments[i]
-            previous = self._segments[i - 1]
-            velocity = previous.get_velocity()
-            trailing.set_velocity(velocity)
     
     def get_head(self):
         return self._segments[0]
 
     def grow_tail(self, number_of_segments):
-        for i in range(number_of_segments):
-            tail = self._segments[-1]
-            velocity = tail.get_velocity()
-            offset = velocity.reverse()
-            position = tail.get_position().add(offset)
-            
-            segment = Actor()
-            segment.set_position(position)
-            segment.set_velocity(velocity)
-            segment.set_text("O")
-            segment.set_color(constants.GREEN)
-            self._segments.append(segment)
+        head = self._segments[0]
+        velocity = head.get_velocity()
+        offset = velocity.reverse()
+        position = head.get_position().add(offset)
+        
+        segment = Actor()
+        segment.set_position(position)
+        segment.set_velocity(Point(0,0))
+        segment.set_text("#")
+        segment.set_color(constants.GREEN)
+        self._segments.append(segment)
 
     def turn_head(self, velocity):
         self._segments[0].set_velocity(velocity)
     
     def _prepare_body(self):
-        #x = 15 
-        x = random.randint(1,300)
-        #y = 15 
-        y = random.randint(1,900)
+        x = int(constants.MAX_X / 4)
+        y = int(constants.MAX_Y / 2)
 
-        for i in range(constants.CYCLE_LENGTH):
+        for i in range(constants.SNAKE_LENGTH):
             position = Point(x - i * constants.CELL_SIZE, y)
-            velocity = Point(0, 0)
-            text = "+" if i == 0 else "O"
+            velocity = Point(1 * constants.CELL_SIZE, 0) if i == 0 else Point(0,0)
+            text = "8" if i == 0 else "#"
             color = constants.YELLOW if i == 0 else constants.GREEN
             
             segment = Actor()
