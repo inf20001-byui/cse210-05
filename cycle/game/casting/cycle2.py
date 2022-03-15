8a1d9ea3c403d86a2c1eccd7389b4ee19a6521c3
Quibly <trippy524@yahoo.com>
2022-03-15 14:58:34 -0600

    Updated comments in cycle classes

--- cycle/game/casting/cycle2.py
+++ cycle/game/casting/cycle2.py
@@ -1,7 +1,4 @@
-#from contextlib import nullcontext
-import random
 import constants
-from game.casting.cycle import Cycle
 from game.casting.actor import Actor
 from game.shared.point import Point
 
@@ -13,22 +10,45 @@ class Cycle2(Actor):
     The responsibility of Cycle is to move itself.
     """
     def __init__(self):
+        """
+        Attributes:
+        _segments []: Holds the segment details
+        _prepare_body(): prepares a new cycle with segments
+        """
         super().__init__()
         self._segments = []
         self._prepare_body()
 
     def get_segments(self):
+        """
+        Returns segment details
+        """
         return self._segments
 
     def move_next(self):
         # move all segments
+        """
+        Sets the movement for each segment
+        """
         for segment in self._segments:
             segment.move_next()
     
     def get_lightCycle(self):
+        """
+        Returns the cycle segment details of the Cycle object
+        """
         return self._segments[0]
 
     def grow_trail(self):
+        """
+        Creates a new segment and appends the segment details to the previous location of the cycle.
+        
+        Attributes:
+        lightCycle: lead segment of the cycle object
+        velocity: direction the lightCycle is heading
+        offset: used to set location of new segment at the previous location of the cycle
+        position: location for new segment
+        """
         lightCycle = self._segments[0]
         velocity = lightCycle.get_velocity()
         offset = velocity.reverse()
@@ -42,9 +62,19 @@ class Cycle2(Actor):
         self._segments.append(segment)
 
     def turn_lightCycle(self, velocity):
+        """
+        Sets the new velocity for the cycle based on key presses
+        """
         self._segments[0].set_velocity(velocity)
         
     def _prepare_body(self):
+        """
+        Creates the body and cycle trail for the Cycle object
+
+        Attributes:
+        x(int): Starting x position for the cycle
+        y(int): Starting y position for the cycle
+        """
         x = int((constants.MAX_X * 3) / 4)
         y = int(constants.MAX_Y - constants.MAX_Y / 8)
 
